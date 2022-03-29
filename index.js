2d253e7f25e8d362e05efdd5a83c2c3820ebfe12
clarababette <clara_coetzee@zoho.com>
2022-03-29 14:12:20 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const templateScript = Handlebars.compile(template);
 
 const container = document.querySelector('.container');
 
-axios.get('http://api-tutor.herokuapp.com/v1/cars').then((response) => {
+axios.get('https://api-tutor.herokuapp.com/v1/cars').then((response) => {
   console.log(response);
   container.innerHTML = templateScript({cars: response.data});
 });
